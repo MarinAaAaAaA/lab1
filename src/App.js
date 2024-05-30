2121cb9258570965113ffbee4148ceb0054d2c09
MarinAaAaAaA <molchanova.03@internet.ru>
2024-05-30 14:19:45 +0300

    lab345

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
-import ResizeModal from './modal'; 
+import ResizeModal from './modal';
+import Toolbar from './toolbar';
+import PipetteInfo from './pipet';
+import CurvesModal from './CurvesModal'; // Импортируем новый компонент
+import { rgbToXyz, rgbToLab, rgbToLch, rgbToOKLch, calculateContrast, calculateAPCA } from './utils';
 
 function App() {
-  const [image, setImage] = useState(); 
-  const [imageURL, setImageURL] = useState(''); 
-  const [color, setColor] = useState(''); 
-  const [position, setPosition] = useState({ x: 0, y: 0 }); 
-  const [scale, setScale] = useState(100); 
-  const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 }); 
-  const [isModalOpen, setIsModalOpen] = useState(false); 
-  const canvasRef = useRef(null); 
-  const fileReader = new FileReader(); 
+  const [image, setImage] = useState(null);
+  const [imageURL, setImageURL] = useState('');
+  const [color, setColor] = useState('');
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [scale, setScale] = useState(100);
+  const [originalDimensions, setOriginalDimensions] = useState({ width: 0, height: 0 });
+  const [displayedDimensions, setDisplayedDimensions] = useState({ width: 0, height: 0 });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCurvesModalOpen, setIsCurvesModalOpen] = useState(false); // Состояние для нового модального окна
+  const [activeTool, setActiveTool] = useState('hand');
+  const [colors, setColors] = useState({ primary: null, secondary: null });
+  const [isPipetteInfoOpen, setIsPipetteInfoOpen] = useState(false);
+  const canvasRef = useRef(null);
+  const fileReader = useRef(new FileReader());
 
- 
-  fileReader.onloadend = () => {
-    setImageURL(fileReader.result);
+  fileReader.current.onloadend = () => {
+    setImageURL(fileReader.current.result);
   };
 
-  
   const handleOnChange = (event) => {
     event.preventDefault();
     const file = event.target.files[0];
     setImage(file);
-    fileReader.readAsDataURL(file);
+    fileReader.current.readAsDataURL(file);
   };
-
 
   const updatePosition = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
     setPosition({ x: offsetX, y: offsetY });
   };
-
 
   const updateColor = (e) => {
     if (!canvasRef.current) return;
@@ -41,29 +46,53 @@ function App() {
     setColor(`rgb(${r}, ${g}, ${b})`);
   };
 
- 
   const handleMouseMove = (e) => {
-    updateColor(e);
-    updatePosition(e);
+    if (activeTool === 'pipette') {
+      updateColor(e);
+      updatePosition(e);
+    }
   };
 
+  const handleCanvasClick = (e) => {
+    if (activeTool !== 'pipette') return;
 
-  const drawImage = (img, scale) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const { offsetX, offsetY } = e.nativeEvent;
+    const [r, g, b] = ctx.getImageData(offsetX, offsetY, 1, 1).data;
+    const color = {
+      rgb: [r, g, b],
+      position: { x: offsetX, y: offsetY },
+      xyz: rgbToXyz([r, g, b]),
+      lab: rgbToLab([r, g, b]),
+      lch: rgbToLch([r, g, b]),
+      oklch: rgbToOKLch([r, g, b])
+    };
+
+    if (e.altKey || e.ctrlKey || e.shiftKey) {
+      setColors({ ...colors, secondary: color });
+    } else {
+      setColors({ ...colors, primary: color });
+    }
+
+    setIsPipetteInfoOpen(true);
+  };
+
+  const drawImage = (img, scale, offsetX, offsetY) => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-  
+
     const scaledWidth = (img.width * scale) / 100;
     const scaledHeight = (img.height * scale) / 100;
-  
+
     canvas.width = scaledWidth;
     canvas.height = scaledHeight;
-  
+
     context.clearRect(0, 0, scaledWidth, scaledHeight);
-    context.drawImage(img, 0, 0, scaledWidth, scaledHeight);
-  
-    setImageDimensions({ width: Math.round(scaledWidth), height: Math.round(scaledHeight) });
+    context.drawImage(img, offsetX, offsetY, scaledWidth, scaledHeight);
+
+    setDisplayedDimensions({ width: Math.round(scaledWidth), height: Math.round(scaledHeight) });
   };
-  
 
   const resizeImage = ({ width, height }) => {
     const canvas = canvasRef.current;
@@ -75,10 +104,9 @@ function App() {
       canvas.height = height;
       context.imageSmoothingEnabled = false;
       context.drawImage(img, 0, 0, width, height);
-      setImageDimensions({ width: width, height: height });
+      setDisplayedDimensions({ width: width, height: height });
     };
   };
-
 
   const saveImage = () => {
     const canvas = canvasRef.current;
@@ -88,56 +116,118 @@ function App() {
     link.click();
   };
 
-  
   useEffect(() => {
     if (!imageURL) return;
     const img = new Image();
     img.src = imageURL;
     img.onload = () => {
-      const initialScale = Math.min(
-        ((window.innerWidth - 100) / img.width) * 100,
-        ((window.innerHeight - 100) / img.height) * 100
-      );
-      setScale(initialScale);
-      drawImage(img, initialScale);
+      setOriginalDimensions({ width: img.width, height: img.height });
+      setDisplayedDimensions({ width: img.width, height: img.height });
+      drawImage(img, scale, 0, 0);
     };
-    img.onerror= () => {
+    img.onerror = () => {
       alert('Ошибка при загрузке изображения. Проверьте URL.');
     };
-  }, [imageURL]);
+  }, [imageURL, scale]);
 
- 
-  useEffect(() => {
-    if (!imageURL) return;
+  const handleKeyDown = (e) => {
+    console.log('Key pressed:', e.key);
+
+    const moveAmount = e.shiftKey ? 20 : e.altKey ? 1 : 10;
+
+    if (activeTool === 'hand') {
+      const canvas = canvasRef.current;
+      const img = new Image();
+      img.src = imageURL;
+
+      img.onload = () => {
+        const { width, height } = canvas;
+        let offsetX = position.x;
+        let offsetY = position.y;
+
+        switch (e.key) {
+          case 'ArrowLeft':
+            offsetX = position.x - moveAmount < 0 ? 0 : position.x - moveAmount;
+            break;
+          case 'ArrowRight':
+            offsetX = position.x + moveAmount > width ? width : position.x + moveAmount;
+            break;
+          case 'ArrowUp':
+            offsetY = position.y - moveAmount < 0 ? 0 : position.y - moveAmount;
+            break;
+          case 'ArrowDown':
+            offsetY = position.y + moveAmount > height ? height : position.y + moveAmount;
+            break;
+          default:
+            break;
+        }
+
+        drawImage(img, scale, offsetX, offsetY);
+        setPosition({ x: offsetX, y: offsetY });
+      };
+    }
+  };
+
+  const applyCurvesCorrection = (lut) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      data[i] = lut[data[i]];     // Red
+      data[i + 1] = lut[data[i + 1]]; // Green
+      data[i + 2] = lut[data[i + 2]]; // Blue
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+  };
+
+  const handlePreviewCurvesCorrection = (lut) => {
+    applyCurvesCorrection(lut);
+  };
+
+  const handleCurvesApply = (lut) => {
+    applyCurvesCorrection(lut);
+    setIsCurvesModalOpen(false);
+  };
+
+  const handleCurvesReset = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
     const img = new Image();
     img.src = imageURL;
-    img.onload = () => drawImage(img, scale);
-  }, [scale]);
-
+    img.onload = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+  };
 
   return (
-    <div className='App'>
-      <div className='uploader'>
+    <div className="App" onKeyDown={handleKeyDown} tabIndex="0">
+      <Toolbar activeTool={activeTool} onSelectTool={setActiveTool} onOpenCurvesModal={() => setIsCurvesModalOpen(true)} />
+      <div className="uploader">
         {image && (
-          <div className='color-info'>
-            <div className='image-size'>
-            Размер изображения: <br />
-            Ширина: {imageDimensions.width}px {image.width && `(${Math.round((imageDimensions.width / image.width) * 100)}%)`}<br />
-            Высота: {imageDimensions.height}px {image.height && `(${Math.round((imageDimensions.height / image.height) * 100)}%)`}
-          </div>
+          <div className="color-info">
+            <div className="image-size">
+              Размер изображения: <br />
+              Ширина: {displayedDimensions.width}px <br />
+              Высота: {displayedDimensions.height}px
+            </div>
             <div>Координаты: <br />X: {position.x}px, <br /> Y: {position.y}px</div>
-            <div className='color'>Цвет: <br /> {color}</div>
+            <div className="color">Цвет: <br /> {color}</div>   
             <div style={{ backgroundColor: color, width: 150, height: 30, marginTop: 10 }}></div>
           </div>
         )}
-        <div className='test'>
+        <div className="test">
           <canvas
             ref={canvasRef}
             onMouseMove={handleMouseMove}
-            className='uploader__canvas'>
-          </canvas>
-          <div>{image ? image.name : ""}</div>
-          <div className='load'>
+            onClick={handleCanvasClick}
+            className="uploader__canvas"
+          ></canvas>
+          <div>{image ? image.name : ''}</div>
+          <div className="load">
             <label htmlFor="loader-button" className="uploader__button">
               Загрузить файл
             </label>
@@ -145,25 +235,29 @@ function App() {
               id="loader-button"
               type="file"
               className="uploader__upload-button"
-              onChange={handleOnChange} />
+              onChange={handleOnChange}
+            />
             <div className="url-uploader">
               <input
                 type="text"
                 placeholder="Введите URL изображения"
                 onChange={(e) => setImageURL(e.target.value)}
-                className="url-input" />
-              <button className='url-button' onClick={setImage}> загрузить </button>
+                className="url-input"
+              />
+              <button className="url-button" onClick={() => fileReader.current.readAsDataURL(image)}>загрузить</button>
             </div>
           </div>
-          <div className='scale-selector'>
+          <div className="scale-selector">
             <label htmlFor="scale">Масштаб:</label>
             <input
               id="scale"
               type="range"
               min="12"
               max="300"
+              stwp ='1'
               value={scale}
               onChange={(e) => setScale(e.target.value)}
+              style={{width:'100%'}}
             />
             <span>{scale}%</span>
           </div>
@@ -174,12 +268,23 @@ function App() {
       <ResizeModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-        imageWidth={imageDimensions.width}
-        imageHeight={imageDimensions.height}
+        imageWidth={originalDimensions.width}
+        imageHeight={originalDimensions.height}
         onResize={resizeImage}
       />
+      <CurvesModal
+        isOpen={isCurvesModalOpen}
+        onClose={() => setIsCurvesModalOpen(false)}
+        onApply={handleCurvesApply}
+        onReset={handleCurvesReset}
+        onPreview={handlePreviewCurvesCorrection}
+      />
+      {isPipetteInfoOpen && (
+        <PipetteInfo colors={colors} onClose={() => setIsPipetteInfoOpen(false)} />
+      )}
     </div>
   );
 }
 
 export default App;
+
